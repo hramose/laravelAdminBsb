a7563eec1cf16efa8554f916190f27be26e5dd2f
Eleazar Resendez <e.resendez@outlook.com>
2018-06-21 16:36:21 -0500

    added csrf-token

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -3,7 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Sign In | {{config('app.name')}}</title>
+
 	<!-- Favicon-->
 	<!-- <link rel="icon" href="../../favicon.ico" type="image/x-icon"> -->
 
